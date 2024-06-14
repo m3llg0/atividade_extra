d089b1e4ed22d119d121bfefbca882c3c0e4dbfd
Melissa <m3lgaldin0@gmail.com>
2024-06-14 10:45:39 -0300

    Segundo commit

--- main.py
+++ main.py
@@ -1,96 +1,94 @@
 from kivy.app import App
+from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.core.window import Window
+import mysql.connector
 
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="users_db"
+)
 
-class TelaLogin(BoxLayout):
+cursor = db.cursor()
+
+cores = {
+    'primaria': '#FFC0CB',  
+    'secundaria': '#000000',  
+    'terciaria': '#FFD1DC'  
+}
+
+kv_code = f"""
+BoxLayout:
+    orientation: 'vertical'
+    padding: 10
+    spacing: 10
+
+    BoxLayout:
+        size_hint_y: None
+        height: sp(40)
+
+        Label:
+            text: 'Usuário:'
+            size_hint_x: None
+            width: sp(100)
+            color: {cores['secundaria']}
+        TextInput:
+            id: usuario
+            multiline: False
+            background_color: {cores['terciaria']}
+            foreground_color: {cores['secundaria']}
+
+    BoxLayout:
+        size_hint_y: None
+        height: sp(40)
+
+        Label:
+            text: 'Senha:'
+            size_hint_x: None
+            width: sp(100)
+            color: {cores['secundaria']}
+        TextInput:
+            id: senha
+            password: True
+            multiline: False
+            background_color: {cores['terciaria']}
+            foreground_color: {cores['secundaria']}
+
+    Button:
+        text: 'Login'
+        background_normal: ''
+        background_color: {cores['primaria']}
+        on_release: app.check_credentials()
+
+    Button:
+        text: 'Cadastrar'
+        background_normal: ''
+        background_color: {cores['primaria']}
+        on_release: app.register_user()
+"""
+
+class LoginScreen(BoxLayout):
     def __init__(self, **kwargs):
-        super(TelaLogin, self).__init__(**kwargs)
-        self.orientation = 'vertical'
-        self.padding = [50, 50]
-        self.spacing = 10
+        super().__init__(**kwargs)
+        self.add_widget(Builder.load_string(kv_code))
 
-        # Adicionando um rótulo
-        self.add_widget(Label(text="L O G I N", font_size=24))
+    def check_credentials(self):
+        username = self.ids.usuario.text
+        password = self.password.text
 
-        # Adicionando campos de entrada de texto
+        query = "SELECT * FROM users WHERE username = %s AND password = %s"
+        cursor.execute(query, (username, password))
+        result = cursor.fetchone()
 
-        self.name_input = TextInput(hint_text='Email', multiline=False)
-        self.add_widget(self.name_input)
-
-        self.password_input = TextInput(hint_text='Senha', multiline=False, password=True)
-        self.add_widget(self.password_input)
-
-        # Botões
-        self.login_button = Button(text='Login', size_hint=(None, None), size=(200, 50))
-        self.login_button.bind(on_press=self.on_login)
-        self.add_widget(self.login_button)
-
-        self.cadastro_button = Button(text='Cadastro', size_hint=(None, None), size=(200, 50))
-        self.cadastro_button.bind(on_press=self.on_cadastro)
-        self.add_widget(self.cadastro_button)
-
-    def on_login(self, *args):
-        self.parent.parent.current = 'Cadastro'
-    
-    def on_cadastro(self, *args):
-        self.parent.parent.current = 'Cadastro'
-
-
-class TelaCadastro(BoxLayout):
-    def __init__(self, **kwargs):
-        super(TelaCadastro, self).__init__(**kwargs)
-        self.orientation = "vertical"
-        self.padding = [50, 50]
-        self.spacing = 10
-
-        # Adicionando um rótulo
-        self.add_widget(Label(text=" .:: C A D A S T R O ::.", font_size=24))
-
-        # Adicionando campos de entrada de texto
-
-        # Email
-        self.add_widget(Label(text="Email", font_size=18))
-        self.email_input = TextInput(hint_text='Digite aqui...', multiline=False)
-        self.add_widget(self.email_input)
-
-        # Senha
-        self.add_widget(Label(text="Senha", font_size=18))
-        self.password_input = TextInput(hint_text='Digite aqui...', multiline=False, password=True)
-        self.add_widget(self.password_input)
-
-        # Adicionando botão de cadastro
-        self.cadastrar_button = Button(text='Cadastrar', size_hint=(None, None), size=(200, 50))
-        self.cadastrar_button.bind(on_press=self.on_cadastrar)
-        self.add_widget(self.cadastrar_button)
-
-    def on_cadastrar(self, instance):
-        # Adicione a lógica de cadastro aqui
-        print("Cadastro button pressed")
-
+        if result:
+            print("Login bem-sucedido!")
+        else:
+            print("Usuário ou senha incorretos.")
 
 class MyApp(App):
     def build(self):
-        Window.size = (300, 500)
-        sm = ScreenManager()
-        tela_login = TelaLogin()
-        tela_cadastro = TelaCadastro()
-
-        screen_login = Screen(name='Login')
-        screen_cadastro = Screen(name='Cadastro')
-
-        screen_login.add_widget(tela_login)
-        screen_cadastro.add_widget(tela_cadastro)
-
-        sm.add_widget(screen_login)
-        sm.add_widget(screen_cadastro)
-
-        return sm
-
+        return LoginScreen()
 
 if __name__ == '__main__':
     MyApp().run()
